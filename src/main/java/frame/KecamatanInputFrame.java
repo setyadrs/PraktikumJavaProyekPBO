2252950524d98489a2646a4cdd6369f583e78a62
setyadrs <setyadrs@gmail.com>
2022-07-05 02:35:35 +0800

    KecamatanInputFrame, KecamatanViewFrame, Relasi Data, ComboBox dan RadioButton

---
+++ src/main/java/frame/KecamatanInputFrame.java
@@ -0,0 +1,166 @@
+package frame;
+import helpers.ComboBoxItem;
+import helpers.Koneksi;
+import javax.swing.*;
+import java.sql.*;
+public class KecamatanInputFrame extends JFrame{
+    private JPanel mainPanel;
+    private JTextField idTextField;
+    private JTextField namaTextField;
+    private JButton simpanButton;
+    private JButton batalButton;
+    private JPanel buttonPanel;
+    private JComboBox kabupatenComboBox;
+    private JRadioButton tipeARadioButton;
+    private JRadioButton tipeBRadioButton;
+    private ButtonGroup  klasifikasiButtonGroup;
+
+    private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public KecamatanInputFrame() {
+        simpanButton.addActionListener(e -> {
+            String nama = namaTextField.getText();
+            if (nama.equals("")) {
+                JOptionPane.showMessageDialog(null,
+                        "Isi Nama Kecamatan",
+                        "Validasi data kosong", JOptionPane.WARNING_MESSAGE);
+                namaTextField.requestFocus();
+                return;
+            }
+            ComboBoxItem item = (ComboBoxItem) kabupatenComboBox.getSelectedItem();
+            int kabupatenId = item.getValue();
+            if(kabupatenId == 0){
+                JOptionPane.showMessageDialog(null,
+                        "Pilih kabupaten",
+                        "Validasi Combobox",JOptionPane.WARNING_MESSAGE);
+                kabupatenComboBox.requestFocus();
+                return;
+            }
+
+            String klasifikasi = "";
+            if(tipeARadioButton.isSelected()){
+                klasifikasi = "Tipe A";
+            }
+            else if (tipeBRadioButton.isSelected()){
+                klasifikasi = "Tipe B";
+            }
+            else{
+                JOptionPane.showMessageDialog(null,
+                        "Pilih klasifikasi",
+                        "Validasi Data Kosong", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+
+            Connection c = Koneksi.getConnection();
+            PreparedStatement ps;
+            try {
+                if (id == 0) {
+                    String cekSQL = "SELECT * FROM kecamatan WHERE nama = ?";
+                    ps = c.prepareStatement(cekSQL);
+                    ps.setString(1, nama);
+                    ResultSet rs = ps.executeQuery();
+                    if (rs.next()) {
+                        JOptionPane.showMessageDialog(null,
+                                "Data sama sudah ada");
+                    } else {
+                        String insertSQL = "INSERT INTO kecamatan (id, nama, kabupaten_id, klasifikasi) " +
+                                "VALUES (NULL, ?, ?, ?)";
+                        ps = c.prepareStatement(insertSQL);
+                        ps.setString(1, nama);
+                        ps.setInt(2, kabupatenId);
+                        ps.setString(3, klasifikasi);
+                        ps.executeUpdate();
+                        dispose();
+                    }
+                } else {
+                    String cekSQL = "SELECT * FROM kecamatan WHERE nama = ? AND id != ?";
+                    ps = c.prepareStatement(cekSQL);
+                    ps.setString(1, nama);
+                    ps.setInt(2, id);
+                    ResultSet rs = ps.executeQuery();
+                    if (rs.next()) {
+                        JOptionPane.showMessageDialog(null,
+                                "Data sama sudah ada");
+                    } else {
+                        String updateSQL = "UPDATE kecamatan SET nama = ?, kabupaten_id = ?, klasifikasi = ? " +
+                                "WHERE id = ?";
+                        ps = c.prepareStatement(updateSQL);
+                        ps.setString(1, nama);
+                        ps.setInt(2, kabupatenId);
+                        ps.setInt(3, id);
+                        ps.setString(3, klasifikasi);
+                        ps.setInt(4, id);
+                        ps.executeUpdate();
+                        dispose();
+                    }
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        batalButton.addActionListener(e -> dispose());
+        kustomisasiKomponen();
+        init();
+    }
+    public void init() {
+        setContentPane(mainPanel);
+        setTitle("Input Kecamatan");
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    public void isiKomponen() {
+        Connection c = Koneksi.getConnection();
+        String findSQL = "SELECT * FROM kecamatan WHERE id = ?";
+        PreparedStatement ps;
+        try {
+            ps = c.prepareStatement(findSQL);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                idTextField.setText(String.valueOf(rs.getInt("id")));
+                namaTextField.setText(rs.getString("nama"));
+                int kabupatenId = rs.getInt("kabupaten_id");
+                for (int i = 0; i < kabupatenComboBox.getItemCount(); i++) {
+                    kabupatenComboBox.setSelectedIndex(i);
+                    ComboBoxItem item = (ComboBoxItem) kabupatenComboBox.getSelectedItem();
+                    if(kabupatenId == item.getValue()){
+                        break;
+                    }
+                }
+                String klasifikasi = rs.getString("klasifikasi");
+                if(klasifikasi != null){
+                    if(klasifikasi.equals("TIPE A")){
+                        tipeARadioButton.setSelected(true);
+                    } else if(klasifikasi.equals("TIPE B")) {
+                        tipeBRadioButton.setSelected(true);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void kustomisasiKomponen() {
+        Connection c = Koneksi.getConnection();
+        String selectSQL = "SELECT * FROM kabupaten ORDER BY nama";
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(selectSQL);
+            kabupatenComboBox.addItem(new ComboBoxItem(0, "Pilih Kabupaten"));
+            while (rs.next()) {
+                kabupatenComboBox.addItem(new ComboBoxItem(
+                        rs.getInt("id"),
+                        rs.getString("nama")));
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        klasifikasiButtonGroup = new ButtonGroup();
+        klasifikasiButtonGroup.add(tipeARadioButton);
+        klasifikasiButtonGroup.add(tipeBRadioButton);
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,12 @@
 import frame.KabupatenViewFrame;
+import frame.KecamatanViewFrame;
 import helpers.Koneksi;
 
 public class main {
     public static void main(String[] args) {
         //Koneksi.getConnection();
-        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+        //KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+        KecamatanViewFrame viewFrame = new KecamatanViewFrame();
         viewFrame.setVisible(true);
     }
 }
